19006047b06e4738d541b024fc836fd408b29557
rea <reasu@pm.me>
2021-12-31 16:07:24 +0100

    clean up unneeded stuff

--- java/src/main/java/codingdojo/Equipment.java
+++ java/src/main/java/codingdojo/Equipment.java
@@ -6,19 +6,11 @@ import java.util.List;
 class Equipment {
     // TODO add a ring item that may be equipped
     //  that may also add damage modifier
-    private Item leftHand;
-    private Item rightHand;
-    private Item head;
-    private Item feet;
-    private Item chest;
-    private List<Item> fullEquipment = Arrays.asList(leftHand, rightHand, head, feet, chest);
+
+    private final List<Item> fullEquipment;
 
     Equipment(Item leftHand, Item rightHand, Item head, Item feet, Item chest) {
-        this.leftHand = leftHand;
-        this.rightHand = rightHand;
-        this.head = head;
-        this.feet = feet;
-        this.chest = chest;
+        fullEquipment = Arrays.asList(leftHand, rightHand, head, feet, chest);
     }
 
 
@@ -30,26 +22,5 @@ class Equipment {
 
     float getDamageModifier() {
         return (float) fullEquipment.stream().mapToDouble(Item::getDamageModifier).sum();
-    }
-
-
-    Item getLeftHand() {
-        return leftHand;
-    }
-
-    Item getRightHand() {
-        return rightHand;
-    }
-
-    Item getHead() {
-        return head;
-    }
-
-    Item getFeet() {
-        return feet;
-    }
-
-    Item getChest() {
-        return chest;
     }
 }
